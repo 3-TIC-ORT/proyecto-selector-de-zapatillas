b1d50f33d07d6b86c4c62204627baf32243f87e4
MatiCABJ12 <50233785@est.ort.edu.ar>
2025-08-25 10:31:07 -0300

    funciona el boton

--- Front end/wireframe_1/ja.js
+++ Front end/wireframe_1/ja.js
@@ -1,1 +1,7 @@
-addEventListener
+let boton_comenzar = document.getElementById ("boton-comenzar")
+
+
+function boton_tocar(){
+console.log("SOY UN BOTN");
+}
+boton_comenzar.addEventListener("click",boton_tocar)
