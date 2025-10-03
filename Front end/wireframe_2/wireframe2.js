bb07d483ef28332c0e12a95b5b70790f7680bb2d
Your Name <you@example.com>
2025-10-02 22:05:47 -0300

    fa, conecte el 2 con el 3, pero utilizando martin como ususairo y 1234 como contraseña

--- Front end/wireframe_2/wireframe2.js
+++ Front end/wireframe_2/wireframe2.js
@@ -1,7 +1,14 @@
-let ingreso = document.getElementById("inicio")
-let usuario = document.getElementById("usuario")
-let contraseña = document.getElementById("contraseña")
-
-function(){
-    
-}
+function login() {
+    let usuario = document.getElementById("nombre").value.trim().toLowerCase();
+    let clave = document.getElementById("contraseña").value.trim();
+    let usuarioCorrecto = "martin";
+    let claveCorrecta = "1234";
+  
+    if (usuario === usuarioCorrecto && clave === claveCorrecta) {
+      document.getElementById("mensaje").innerText = "¡bien ahi wacho!";
+      window.location.href = "../wireframe_3/html3.html"
+    } else {
+      document.getElementById("mensaje").innerText = "Usuario o contraseña incorrectos.";
+    }
+  }
+  
