0945be1bcb4cf7b58ea4bf9b848b51aa6d2379eb
Somonolo <simonfejler@gmail.com>
2025-10-09 10:29:40 -0300

    tamos terminando el menu

--- Front end/wireframe_3/js3.js
+++ Front end/wireframe_3/js3.js
@@ -6,3 +6,9 @@ function cambiar() {
 }
 
 menu_lateral.addEventListener("click", cambiar);
+window.addEventListener('click', function(e) {
+
+    if (barras.classList.contains('visible') && !barras.contains(e.target) && !menu_lateral.contains(e.target)) {
+        barras.classList.remove('visible');
+    }
+});
