7168dea3c38cb565005954ce9578afe8f2744e19
Your Name <you@example.com>
2025-10-26 20:25:02 -0300

    hice los 10 pantallas de cuestionario

---
+++ Front end/cuestionario/pregunta_7/pregunta_7.js
@@ -0,0 +1,14 @@
+let menu_lateral = document.getElementById("menu");
+let barras = document.getElementById("lateral");
+
+function cambiar() {
+    barras.classList.toggle("visible"); 
+}
+
+if (menu_lateral) menu_lateral.addEventListener("click", cambiar);
+window.addEventListener('click', function(e) {
+    if (barras && barras.classList.contains('visible') && !barras.contains(e.target) && !menu_lateral.contains(e.target)) {
+        barras.classList.remove('visible');
+    }
+});
+
