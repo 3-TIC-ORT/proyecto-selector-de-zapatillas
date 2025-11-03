6414a3ec667ddc01bac5a922688637febab75117
Your Name <you@example.com>
2025-11-03 20:55:16 -0300

    hecho el boton de cerrar la sesion, falta temrinae la facha, pero bue, me re costo qsy, mati wayne cogido

--- Front end/Tab diseño/tab.js
+++ Front end/Tab diseño/tab.js
@@ -7,8 +7,22 @@ function cambiar() {
 
 menu_lateral.addEventListener("click", cambiar);
 window.addEventListener('click', function(e) {
-
     if (barras.classList.contains('visible') && !barras.contains(e.target) && !menu_lateral.contains(e.target)) {
         barras.classList.remove('visible');
     }
+});
+
+
+const cerrarImg = document.getElementById("cerrar"); 
+const inputEscondido = document.getElementById("input_escondido");
+const cancelButton = document.getElementById("cancel-button");
+
+
+cerrarImg.addEventListener("click", () => {
+    inputEscondido.classList.remove("hidden");
+});
+
+
+cancelButton.addEventListener("click", () => {
+    inputEscondido.classList.add("hidden");
 });
