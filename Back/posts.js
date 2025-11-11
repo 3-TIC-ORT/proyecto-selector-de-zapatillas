a9be1c2baff7f76d8881e7a228e161af38257c91
Somonolo <simonfejler@gmail.com>
2025-11-11 11:23:10 -0300

    hice algunas boludeces

--- Back/posts.js
+++ Back/posts.js
@@ -3,10 +3,10 @@ import { subscribeGETEvent, subscribePOSTEvent, realTimeEvent, startServer} from
 
 import {InicioSesion} from "./FUNCIONES.js";
 import {Registro} from "./FUNCIONES.js";
-import {Filtradozapatillas} from "./FUNCIONES.js";
+
 
 subscribePOSTEvent ("iniciarsesion", InicioSesion);
 subscribePOSTEvent ("registro", Registro);
-subscribePOSTEvent ("filtros", Filtradozapatillas);
+
 
 startServer();
